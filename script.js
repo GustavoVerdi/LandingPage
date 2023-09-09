a18abc6069cfb7fb5e2377862418d7328e8b1574
Gustavo Verdi <140762138+GustavoVerdi@users.noreply.github.com>
2023-09-09 13:35:19 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-$('.navTrigger').on('click touchstart', function () {
-    console.log("Botão clicado"); // Adicione esta linha
-    $(this).toggleClass('active');
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
-});
